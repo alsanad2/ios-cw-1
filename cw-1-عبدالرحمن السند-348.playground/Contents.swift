68d0abe4066df98128278bd969cda70bdf5c2a6f
alsanad2 <alsanad2@outlook.com>
2020-06-14 04:18:47 -0600

    done

---
+++ cw-1-عبدالرحمن السند-348.playground/Contents.swift
@@ -0,0 +1,33 @@
+
+   //part1
+  let name =  "عبدالرحمن السند"
+    
+var age = 18
+var height = 1.63
+var hobby1 = "football"
+var hobby2 =  " horse riding"
+
+    print( "my name is: ", name, " , and my age is \(age), and my height is : \(height), my hobbies are : \(hobby1), \(hobby2))")
+ 
+
+    
+    
+    
+ //part2
+ // mohammed
+ var friendName = "mohammed"
+ var friendAge = 18
+    
+    var  friendHeight = 1.64
+    var  friendHobby1 = "football"
+    var  friendHobby2 = "swimming"
+    
+    //comparing
+    name != friendName
+    age == friendAge
+    height > friendHeight
+    
+    hobby1 == friendHobby1
+    
+    hobby2 != friendHobby2
+    
